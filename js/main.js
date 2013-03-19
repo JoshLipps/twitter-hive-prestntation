570fbba99a49128c9aa57d22cea1dabf650cb78d
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 16:32:22 -0700

    google graph api

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
         // Create and populate the data table.
         var data = google.visualization.arrayToDataTable([
           ["firstHashtag","Count"],
-["KCA","61964"],
-["RT","24213"],
-["TeamFollowBack","20970"],
-["gameinsight","19220"],
-["VoteWhatMakesYouBeautiful","17585"],
-["androidgames","15403"],
-["android","14419"],
-["TFBJP","14419"],
-["90sBabyFollowTrain","11552"],
-["TFB","9528"]
-        ]);
+          ["KCA","61964"],
+          ["RT","24213"],
+          ["TeamFollowBack","20970"],
+          ["gameinsight","19220"],
+          ["VoteWhatMakesYouBeautiful","17585"],
+          ["androidgames","15403"],
+          ["android","14419"],
+          ["TFBJP","14419"],
+          ["90sBabyFollowTrain","11552"],
+          ["TFB","9528"]
+          ]);
       
         // Create and draw the visualization.
         var chart = new google.visualization.ColumnChart(document.getElementById('visualization'));
