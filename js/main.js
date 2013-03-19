ac9328885206d9594f44d60d56ea3b8d1a1dcea1
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 16:28:41 -0700

    google graph api

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
       function drawVisualization() {
         // Create and populate the data table.
         var data = google.visualization.arrayToDataTable([
-          ['KCA', 'RT', 'ILikeTODance', 'Greece'],
-          [1901067,    517206,    916965,    1056036]
+          ["firstHashtag","Count"],
+["KCA","61964"],
+["RT","24213"],
+["TeamFollowBack","20970"],
+["gameinsight","19220"],
+["VoteWhatMakesYouBeautiful","17585"],
+["androidgames","15403"],
+["android","14419"],
+["TFBJP","14419"],
+["90sBabyFollowTrain","11552"],
+["TFB","9528"]
         ]);
       
         // Create and draw the visualization.
-        new google.visualization.BarChart(document.getElementById('visualization')).
-            draw(data,
-                 {title:"Popular Hashtags over 24 hours,
-                  width:600, height:400,
-                  vAxis: {title: "Last 24 Hours"},
-                  hAxis: {title: "Tweets"}}
-            );
+        var chart = new google.visualization.ColumnChart(document.getElementById('visualization'));
+        var options = {
+          title: 'Company Performance',
+          hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}
+        };
+        
+        chart.draw(data, options);
       }
       
 
