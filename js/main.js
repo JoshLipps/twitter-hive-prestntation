54b5469b0850ea0f8248eb529f564657959e42ef
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 16:18:01 -0700

    google graph api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+      function drawVisualization() {
+        // Create and populate the data table.
+        var data = google.visualization.arrayToDataTable([
+          ['KCA', 'RT', 'ILikeTODance', 'Greece'],
+          [1901067,    517206,    916965,    1056036]
+        ]);
+      
+        // Create and draw the visualization.
+        new google.visualization.BarChart(document.getElementById('visualization')).
+            draw(data,
+                 {title:"Popular Hashtags over 24 hours,
+                  width:600, height:400,
+                  vAxis: {title: "Last 24 Hours"},
+                  hAxis: {title: "Tweets"}}
+            );
+      }
+      
 
+      google.setOnLoadCallback(drawVisualization);
