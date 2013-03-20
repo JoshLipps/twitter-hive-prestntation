1f396e267ed87411bf0cc5644d838169f2e0dbe3
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:00:24 -0700

    added Jquery getjson from fs

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
       function drawVisualization() {
         // Create and populate the data table.
   
+        $.getJSON('data/hashtag',function(hashtags){
+          var data = google.visualization.arrayToDataTable(hashtags);
+          var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
+          chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
 
-
-          var data = google.visualization.arrayToDataTable([['Hashtag','Count'],
-['KCA',61964],
-['RT',24213],
-['TeamFollowBack',20970],
-['gameinsight',19220],
-['VoteWhatMakesYouBeautiful',17585],
-['androidgames',15403],
-['android',14419],
-['TFBJP',14419],
-['90sBabyFollowTrain',11552],
-['TFB',9528]]);
-
-      
-        var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
-        chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
-
+        });
       }
       
-
-      google.setOnLoadCallback(drawVisualization);
+$(document).ready(function() {
+    google.setOnLoadCallback(drawVisualization);
+  }
 
 
       
