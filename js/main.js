9ef22ff11a5baf923ba0fa8e55ea42ee6e8d6e23
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:45:20 -0700

    Fixed Jquery bug

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ function drawVisualization(){
 
   $.getJSON('data/hashtags',function(hashtags){
     var data = google.visualization.arrayToDataTable(hashtags);
-
-    console.log("Data's: "+hashtags);
+    console.log("Data");
+    console.log("Data's: "+ hashtags);
     
     var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
-    chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
+    chart.draw(data, {width: 900, height: 400, min: 0);
   });
 }
 
