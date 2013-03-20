7d14a202057e77870af2db5ee467c1ed97bc2355
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:24:22 -0700

    added Jquery getjson from fs

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ $(document).ready(function() {
 }
 
 function drawVisualization() {
-  $.getJSON('data/hashtags',function(hashtags){
-    var data = google.visualization.arrayToDataTable(hashtags);
-    var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
-    chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
-  });
+
 }
 
 
