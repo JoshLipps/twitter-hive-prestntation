106230fe1a65e4f8af8bebb25d6deb2b4f0f8cd9
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:28:53 -0700

    Fixed Jquery bug

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
 function drawVisualization(){
   $.getJSON('data/hashtags',function(hashtags){
     var data = google.visualization.arrayToDataTable(hashtags);
+
+    console.log(data);
+    
     var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
     chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
   });
