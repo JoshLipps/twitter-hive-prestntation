4045d784ff15c26a72817455124cde382798c009
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 17:19:06 -0700

    google graph api

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
       function drawVisualization() {
         // Create and populate the data table.
-        var data = google.visualization.arrayToDataTable([
-          ['hashtags','count'],
-          ["KCA","RT"],
-          ["61964","24213"
+        var data = google.visualization.arrayToDataTable([["hashtags","count"],
           ["TeamFollowBack","20970"],
           ["gameinsight","19220"],
           ["VoteWhatMakesYouBeautiful","17585"],
@@ -11,8 +8,7 @@
           ["android","14419"],
           ["TFBJP","14419"],
           ["90sBabyFollowTrain","11552"],
-          ["TFB","9528"]
-          ]);
+          ["TFB","9528"]]);
       
         var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
         chart.draw(data, {width: 400, height: 240, min: 0,isVertical: true});
