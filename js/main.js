f0ae55939b781ffe4feeb3ed0327223460ed4336
Josh Lipps <josh.lipps@gmail.com>
2013-03-20 01:53:22 -0700

    Adding new graph

--- js/main.js
+++ js/main.js
@@ -30,10 +30,11 @@ function drawLines() {
                 if(hashs.indexOf(rows[i][0]) === -1) {
                   hashs.push(rows[i][0]);
                 }
-                if(!parts[rows[i][1]]) {
-                  parts[rows[i][1]]=[];
-                  parts[rows[i][1]].push(parseInt(rows[i][1]);
-                  }
+                if(true) {}
+                 //!parts[rows[i][1]] 
+               //parts[rows[i][1]]=[];
+                  //parts[rows[i][1]].push(parseInt(rows[i][1]);
+                  //}
                 parts[rows[i][1]].push(parseInt(rows[i][2]);
         }
         
