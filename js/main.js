259ebd4dda04b689aa15ab11c70b06a5afd40e12
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:26:12 -0700

    Fixed Jquery bug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
     google.setOnLoadCallback(drawVisualization);
+});
+
+function drawVisualization(){
+  $.getJSON('data/hashtags',function(hashtags){
+    var data = google.visualization.arrayToDataTable(hashtags);
+    var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
+    chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
+  });
 }
-
-
 
 
       
