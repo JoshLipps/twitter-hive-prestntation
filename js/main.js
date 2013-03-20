fda334ed58e9ea16a8ca4eb0d78ed0a9774ca54d
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 17:25:30 -0700

    google graph api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
       function drawVisualization() {
         // Create and populate the data table.
-        var data = google.visualization.arrayToDataTable([["hashtags","count"],
-          ["TeamFollowBack","20970"],
-          ["gameinsight","19220"],
-          ["VoteWhatMakesYouBeautiful","17585"],
-          ["androidgames","15403"],
-          ["android","14419"],
-          ["TFBJP","14419"],
-          ["90sBabyFollowTrain","11552"],
-          ["TFB","9528"]]);
+  
+
+
+          var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales'],
+          ['2004',  1000],
+          ['2005',  1170],
+          ['2006',  660],
+          ['2007',  1030]
+        ]);
+
       
         var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
         chart.draw(data, {width: 400, height: 240, min: 0,isVertical: true});
