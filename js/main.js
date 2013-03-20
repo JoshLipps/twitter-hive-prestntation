554b6f999544290c7558c64efb3a71e970b65c8a
Josh Lipps <josh.lipps@gmail.com>
2013-03-19 19:04:10 -0700

    fixed css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-      function drawVisualization() {
-        // Create and populate the data table.
-  
-        $.getJSON('data/hashtag',function(hashtags){
-          var data = google.visualization.arrayToDataTable(hashtags);
-          var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
-          chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
+function drawVisualization() {
+  $.getJSON('data/hashtag',function(hashtags){
+    var data = google.visualization.arrayToDataTable(hashtags);
+    var chart = new google.visualization.ImageBarChart(document.getElementById('visualization'));
+    chart.draw(data, {width: 900, height: 400, min: 0,isVertical: true});
 
-        });
-      }
+  });
+}
       
 $(document).ready(function() {
     google.setOnLoadCallback(drawVisualization);
